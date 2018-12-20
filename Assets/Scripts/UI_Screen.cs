6786efade435a7fc2ec743c30248437ece36be38
AlexSerodio <alequis01@gmail.com>
2018-12-20 19:55:02 -0200

    Add main screen and fade functions

--- Assets/Scripts/UI_Screen.cs
+++ Assets/Scripts/UI_Screen.cs
@@ -1,30 +1,64 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 namespace ReusableUI
 {
+	[RequireComponent(typeof(Animator))]
+	[RequireComponent(typeof(CanvasGroup))]
     public class UI_Screen : MonoBehaviour {
 
 		#region Variables
+		[Header("Main Properties")]
+		public Selectable m_StartSelectable;	// the element that will start highlighted
+
+		[Header("Screen Events")]
+		public UnityEvent onScreenStart = new UnityEvent();
+		public UnityEvent onScreenClose = new UnityEvent();
+
+		private Animator animator;
 
 		#endregion
 
 
 
 		#region Main Methods
-		// Use this for initialization
+		
 		void Start () {
-			
+			animator = GetComponent<Animator>();
+
+			if(m_StartSelectable)
+				EventSystem.current.SetSelectedGameObject(m_StartSelectable.gameObject);
 		}
 		
-		// Update is called once per frame
-		void Update () {
-			
-		}
 		#endregion
 
 	
 
 		#region Helper Methods
+
+		public virtual void StartScreen() {
+			if(onScreenStart != null) {
+				onScreenStart.Invoke();
+			}
+
+			HandleAnimator("show");
+		}
+
+		public virtual void CloseScreen() {
+			if(onScreenClose != null) {
+				onScreenClose.Invoke();
+			}
+
+			HandleAnimator("hide");
+		}
+
+		private void HandleAnimator(string aTrigger) {
+			if(animator) {
+				animator.SetTrigger(aTrigger);
+			}
+		}
 
 		#endregion
 	}
--- Assets/Scripts/UI_System.cs
+++ Assets/Scripts/UI_System.cs
@@ -1,27 +1,92 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace ReusableUI
 {
     public class UI_System : MonoBehaviour {
 
 		#region Variables
+		
+		[Header("System Events")]
+		public UnityEvent onSwitchScreen = new UnityEvent();
 
+		[Header("Fader Properties")]
+		public Image m_Fader;
+		public float m_FadeInDuration = 1f;
+		public float m_FadeOutDuration = 1f;
+
+		private Component[] screems = new Component[0];
+		private UI_Screen currentScreen;
+		private UI_Screen previousScreen;
+		
+		public UI_Screen CurrentScreen {
+			get { return currentScreen; }
+		}
+
+		public UI_Screen PreviousScreen {
+			get { return previousScreen; }
+		}
 		#endregion
 
 		#region Main Methods
 		// Use this for initialization
 		void Start () {
-			
+			screems = GetComponentsInChildren<UI_Screen>(true);
+		
+			if(m_Fader)
+				m_Fader.gameObject.SetActive(true);
+
+			FadeIn();
 		}
 		
-		// Update is called once per frame
-		void Update () {
-			
-		}
+		
 		#endregion
 
 		#region Helper Methods
 
+		public void SwitchScreen(UI_Screen aScreen) {
+			if(aScreen) {
+				if(currentScreen) {
+					// currentScreen.Close();
+					previousScreen = currentScreen;
+				}
+
+				currentScreen = aScreen;
+				currentScreen.gameObject.SetActive(true);
+				// currentScreen.StartScreen();
+
+				if(onSwitchScreen != null)
+					onSwitchScreen.Invoke();
+			}
+		}
+
+		public void FadeIn() {
+			if(m_Fader) {
+				m_Fader.CrossFadeAlpha(0f, m_FadeInDuration, false);
+			}
+		}
+
+		public void FadeOut() {
+			if(m_Fader) {
+				m_Fader.CrossFadeAlpha(0f, m_FadeOutDuration, false);
+			}
+		}
+
+		public void GoToPreviousScreen() {
+			if(previousScreen)
+				SwitchScreen(previousScreen);
+		}
+
+		public void LoadScreen(int sceneIndex) {
+			StartCoroutine(WaitToLoadScene(sceneIndex));
+		}
+
+		private IEnumerator WaitToLoadScene(int sceneIndex) {
+			
+			yield return null;
+		}
 		#endregion
 	}
 }
